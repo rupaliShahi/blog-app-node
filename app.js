e6ff4080f182facd822ce681cc1e70a67418227f
rupaliShahi <rupali_928@outlook.com>
2023-02-09 20:52:35 +0000

    added view engine ejs

--- app.js
+++ app.js
@@ -4,20 +4,31 @@ const app = express();
 
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'test1', snippet: 'Lorem ipsum test1'},
+        {title: 'test2', snippet: 'Lorem ipsum test2'},
+        {title: 'test3', snippet: 'Lorem ipsum test3'},
+    ];
+    res.render('index', { title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About'});
 });
 
-// redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog'});
 })
+// redirect
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// })
 
 // 404
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404'});
 })
